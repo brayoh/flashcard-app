890e6066f45bb212ec5c21092b25223bcaa311d6
brayoh <bkuriakb@gmail.com>
2017-04-28 15:39:38 +0300

    add nested routing for child components

--- src/index.js
+++ src/index.js
@@ -6,20 +6,30 @@ import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import * as reducers from './reducers/reducers';
 import App from './components/App';
-
+import * as localStore from './stores/localStorage';
+import VisibleCards from './components/VisibleCards';
+import NewCardModal from './components/NewCardModal';
 
 reducers.routing = routerReducer;
-const store = createStore(combineReducers( reducers ));
+
+const store = createStore(combineReducers(reducers), localStore.get());
 const history = syncHistoryWithStore(browserHistory, store)
 
-function run() {
+function init() {
+    let state = store.getState();
+    localStore.set(state, ['decks', 'cards'])
+
     ReactDOM.render(
         <Provider store={store}>
             <Router history={history}>
-                <Route path='/' component={App} />
+                <Route path='/' component={App}>
+                    <Route path='/deck/:deckId' component={VisibleCards}>
+                        <Route path='/deck/:deckId/new' component={NewCardModal}></Route>
+                    </Route>
+                </Route>
             </Router>
         </Provider>, document.getElementById('root'));
 }
 
-run();
-store.subscribe(run);
+init();
+store.subscribe(init);
