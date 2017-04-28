8b6f37199c8cacade0711b499990ce413f90c5db
brayoh <bkuriakb@gmail.com>
2017-04-28 15:38:36 +0300

    add toolbar component

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,4 @@
-export const addDeck = name => ({type: 'ADD_DECK', data: name});
-export const showDeck = () => ({type: 'SHOW_ADD_DECK'});
-export const hideDeck = () => ({type: 'HIDE_ADD_DECK'});
+export const showAddDeck = () => ({type: 'SHOW_ADD_DECK' });
+export const addDeck = name => ({type: 'ADD_DECK', data: name });
+export const hideAddDeck = () => ({type: 'HIDE_ADD_DECK' });
+export const addCard = card => ({type: 'ADD_CARD', data: card });
